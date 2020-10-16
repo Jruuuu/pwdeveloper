8df7d0b7381f6936c7d326fc3b535807ec9f551a
Jruuuu <71367882+Jruuuu@users.noreply.github.com>
2020-10-16 00:20:38 -0700

    added confirm arrays from user inputs

--- assets/script.js
+++ assets/script.js
@@ -1,74 +1,96 @@
 // // Assignment Code
-// var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
-// // Write password to the #password input
-// //function writePassword() {
-//  var password = generatePassword();
-//  var passwordText = document.querySelector("#password");
+// Write password to the #password input
+function writePassword() {
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+// When Click generate alerts a series of questions for password criteria
 
+function generatePassword() {
+  // make variable characlength
+  var characlength = prompt("Hello, Please enter length of password. 8-128");
 
+  // Prompt the length of password: min range 8 - 128 characters
 
+  if (characlength === null || characlength < 8 || characlength > 128) {
+    alert("Number needs to between 8-128 try again")
+  }
+  //alert type of characters to use
+  //Confirm lowercase
+  var lower = confirm("Would you like lowercase letters in your password?")
 
-//  passwordText.value = password;
+  //confirm uppercase
+  var upper = confirm("Would you like uppercase letters in your password?")
 
+  //confirm numeric
+  var numbers = confirm("Would you like numbers in your password?")
 
+  //confirm special characters
+  var specials = confirm("Would you like special characters in your password?")
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+  // each group should have its own function
 
+  // for lowercase i have to use const to set or cannot be changed, var did not work research to be done.
+  function lowerRandom() {
+    const lower = 'abcdefghijklmnopqrstuvwxyz';
+    return lower[Math.floor(Math.random() * lower.length)];
+  }
 
-
-//When Click generate alerts a series of questions for password criteria
-//make variable characlength
-//Prompt the length of password: min range 8 - 128 characters
-
-function generateBtn() {
-var characlength = prompt("Hello, Please enter length of password. 8-128");
-
-if (characlength === null || characlength < 8 || characlength > 128) {
-    alert("Number needs to between 8-128 try again")    
-} 
-//   //alert type of characters to use
-//   //Confirm lowercase
-var  lower = confirm("Would you like lowercase letters in your password?")
-
-//confirm uppercase
-var  upper = confirm("Would you like uppercase letters in your password?")
-
-//confirm numeric
-var numbers = confirm("Would you like numbers in your password?")
-
-//confirm special characters
-var specials = confirm("Would you like special characters in your password?")
-
-// each group should have its own function
-  //if only one confirm
-
-    // for lowercase i have to use const to set or cannot be changed, var did not work research to be done.
-function lowerRandom() {
-  const lower = 'abcdefghijklmnopqrstuvwxyz';
-        return lower[Math.floor(Math.random() * lower.length)];
-     }
-
-     console.log(lowerRandom())
-       //just for uppercase
-function upperRandom() {
-  const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+   //just for uppercase
+  function upperRandom() {
+    const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     return upper[Math.floor(Math.random() * upper.length)];
- }
+  }
 
-  
-       //index just for numbers
-function numberRandom() {
-  const number = '123456789';
+
+  //index just for numbers
+  function numberRandom() {
+    const number = '123456789';
     return number[Math.floor(Math.random() * number.length)];
- }
+  }
 
-       //index just for special characters
-function specialRandom() {
-  const special = '!"#$%&()*+,-./:;<=>?@[';
+  //index just for special characters
+  function specialRandom() {
+    const special = '!"#$%&()*+,-./:;<=>?@[';
     return special[Math.floor(Math.random() * special.length)];
- }
+  }
+
+//use index for combos
+//TWO CONFIRMS
+//lowercase + uppercase
+var confirmLowerUpper = [lowerRandom, upperRandom];
+
+//lowercase + numbers
+var confirmLowerNumber = [lowerRandom, upperRandom];
+
+//lowercase + specialCharacters
+var confirmLowerSpecial =[lowerRandom, upperRandom];
+
+//THREE CONFRIMS
+//lowercase + uppercase + numbers
+var confirmLowerUpperNumber =[lowerRandom, upperRandom, numberRandom];
+
+//lowercase + specialCharacters + numbers
+var confirmLowerSpecialNumber = [lowerRandom,specialRandom, numberRandom];
+
+//uppercase + specialCharacters + numbers
+
+//FOUR CONFIRMS
+//lowercase + uppercase + numbers + specialCharacters
+
+
+
+
+passwordText.value = password;
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
 
 
 //use index for combos
