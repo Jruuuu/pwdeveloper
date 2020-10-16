7f44bfee867b279e8c403cfb3d751df655834eeb
Jruuuu <71367882+Jruuuu@users.noreply.github.com>
2020-10-15 18:33:25 -0700

    added alert confirm to javascript

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,19 @@
-// Assignment Code
+// // Assignment Code
 // var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-//function writePassword() {
+// // Write password to the #password input
+// //function writePassword() {
 //  var password = generatePassword();
 //  var passwordText = document.querySelector("#password");
 
- // passwordText.value = password;
 
 
 
-// Add event listener to generate button
+//  passwordText.value = password;
+
+
+
+// // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
 
 
@@ -19,37 +22,35 @@
 //make variable characlength
 //Prompt the length of password: min range 8 - 128 characters
 
-// function generateBtn() {
-// var characlength = prompt("Hello, Please enter length of password. 8-128");
+function generateBtn() {
+var characlength = prompt("Hello, Please enter length of password. 8-128");
 
-// if (characlength === null || characlength < 8 || characlength > 128) {
-//     alert("Number needs to between 8-128 try again")    
-// } 
+if (characlength === null || characlength < 8 || characlength > 128) {
+    alert("Number needs to between 8-128 try again")    
+} 
 //   //alert type of characters to use
 //   //Confirm lowercase
-//   else{
-//     confirm("Would you like lowercase letters in your password?")
-//   }
-//   }
-
-
-
-//alert type of characters to use
-//Confirm lowercase
+var  lower = confirm("Would you like lowercase letters in your password?")
 
 //confirm uppercase
-//confirm numeric
-//confirm special characters
+var  upper = confirm("Would you like uppercase letters in your password?")
 
+//confirm numeric
+var numbers = confirm("Would you like numbers in your password?")
+
+//confirm special characters
+var specials = confirm("Would you like special characters in your password?")
 
 // each group should have its own function
   //if only one confirm
-  
+
     // for lowercase i have to use const to set or cannot be changed, var did not work research to be done.
 function lowerRandom() {
   const lower = 'abcdefghijklmnopqrstuvwxyz';
         return lower[Math.floor(Math.random() * lower.length)];
      }
+
+     console.log(lowerRandom())
        //just for uppercase
 function upperRandom() {
   const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
